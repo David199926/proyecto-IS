1754c2ba2fe04494bec3961aed1d999e6de4d788
nazdum <64675616+nazdum@users.noreply.github.com>
2021-05-15 21:46:34 -0500

    Update App.js
    se añadió el componente ActividadForanea.js al Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import LogIn from './LogIn/LogIn';
 import CrearActividad from './CrearActividad/CrearActividad';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ActividadForanea } from './ActividadForanea/ActividadForanea';
 
 function App() {
   return (
     <Router>
       <Route path="/" exact component={LogIn} />
       <Route path="/crear" component={CrearActividad} />
+      <Route path="/actividad" component={ActividadForanea }/>
+
     </Router>
   );
 }
