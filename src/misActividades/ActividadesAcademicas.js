80ee32c89203dd2c61621aec2d29e7051c48188b
nazdum <64675616+nazdum@users.noreply.github.com>
2021-05-24 20:31:06 -0500

    Update ActividadesAcademicas.js
    se actualizo el componente para que extraiga las actividades desde la base de datos y las renderice

--- src/misActividades/ActividadesAcademicas.js
+++ src/misActividades/ActividadesAcademicas.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useEffect ,useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,35 +7,56 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Button, IconButton, Typography } from '@material-ui/core';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
+import { Typography } from '@material-ui/core';
+
+import {EditarActividad } from './EditarActividad';
+import {EliminarActividad } from './EliminarActividad';
 
 export const ActividadesAcademicas = () => {
     
-    const useStyles = makeStyles({
-        table: {
-          minWidth: 650,
-        },
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  const classes = useStyles();
+
+  const [state, setState] = useState([{}]);
+
+
+  const getActividadesAcademicas = async () => {
+    const url = 'http://localhost:4000/mis-actividades/academicas';
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const auxiliarData = [];
+
+    for (let i = 0; i < data.length; i++) {
+      auxiliarData.push({
+        nombre: data[i].data.nombre, tipo: data[i].data.tipo,
+        progreso: data[i].data.progreso, fecha: data[i].data.fecha,
+        acciones: <><EditarActividad /> <EliminarActividad idActividad={data[i].id}/> </>
       });
-      
-      function createData(nombre, tipo, progreso, fecha, acciones) {
-        return { nombre, tipo, progreso, fecha, acciones};
-      }
-      
-      const rows = [
-        createData('Actividad 1', 'Tipo 1', 10, '24/04/2021'),
-        createData('Actividad 2', 'Tipo 2', 20, '24/04/2021'),
-        createData('Actividad 3', 'Tipo 3', 30, '24/04/2021'),
-     
-      ];
 
-      const classes = useStyles();
+    }
 
-    return (
-       <>
-       <Typography color="textPrimary" variant="h3">Academicas</Typography>
-        <TableContainer component={Paper}>
+    setState(auxiliarData);
+
+  }
+
+  useEffect(() => {
+    getActividadesAcademicas();
+  }, []);
+
+
+
+  const rows = state;
+
+  return (
+    <>
+      <Typography color="textPrimary" variant="h3">Profesionales</Typography>
+      <TableContainer component={Paper}>
         <Table className={classes.table} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
@@ -55,12 +76,12 @@ export const ActividadesAcademicas = () => {
                 <TableCell align="right">{row.tipo}</TableCell>
                 <TableCell align="right">{row.progreso}</TableCell>
                 <TableCell align="right">{row.fecha}</TableCell>
-                <TableCell align="right"><><Button startIcon={<EditIcon/>}/> <Button startIcon={<DeleteIcon/>}/> </></TableCell>
+                <TableCell align="right">{row.acciones}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-       </>
-    )
+    </>
+  )
 }
