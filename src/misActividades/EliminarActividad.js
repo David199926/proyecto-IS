e394cab81aa6696827e3ac0cdb5ce15d3ba7b2b1
nazdum <64675616+nazdum@users.noreply.github.com>
2021-05-24 20:30:19 -0500

    Update EliminarActividad.js
    se actualizó el componente para que pueda hacer una petición al backend para eliminar la actividad seleccionada

--- src/misActividades/EliminarActividad.js
+++ src/misActividades/EliminarActividad.js
@@ -1,24 +1,26 @@
-import { Button  } from '@material-ui/core'
-import React , {useState , useEffect} from 'react'
+import { Button } from '@material-ui/core'
+import React, { useEffect } from 'react'
 import DeleteIcon from '@material-ui/icons/Delete'
 
-export const EliminarActividad = ({idActividad}) => {
+export const EliminarActividad = ({ idActividad }) => {
 
-    const [state,setState] = useState(0);
+    const eliminar = async () => {
 
-    useEffect(()=>{
-        setState(idActividad);
-    } ,[idActividad]);
 
-    useEffect(()=>{
-        setState(idActividad);
-    } ,[]);
+        const url = 'http://localhost:4000/mis-actividades/delete';
+        const options ={
+            method: 'POST',
+            headers: {'Content-type': 'application/json' ,'Accept': 'application/json'},
+            body: JSON.stringify({id : idActividad})
+        }
+        
+        fetch(url,options)
+            .then(response => response.json())
+            .then(response => {console.log(response);})
+            .catch(err => console.log(err));
 
-    const eliminar = () =>{
-        /*
-        * Logica para eliminar la actividad de la BD
-        */
     }
 
-    return <Button onClick={eliminar} startIcon={<DeleteIcon/>}/>      
+    return <Button onClick={eliminar} startIcon={<DeleteIcon />} />
 }
+
