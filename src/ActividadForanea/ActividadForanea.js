042d5814c463014589358f8b1b2fe2c89d8ddba1
Bruno <david.lopez_pe@uao.edu.co>
2021-06-10 15:00:58 -0500

    terminado detalles de actividad

--- src/ActividadForanea/ActividadForanea.js
+++ src/ActividadForanea/ActividadForanea.js
@@ -6,7 +6,7 @@ import { DescripCionActividad } from './DescripCionActividad';
 import { InformacionActividad } from './InformacionActividad';
 import { SolicitarColaboracion } from './SolicitarColaboracion';
 import { TemasRelacionados } from './TemasRelacionados';
-import {BACKEND_URL} from '../Constants/constants'
+import { BACKEND_URL } from '../Constants/constants'
 import { useParams } from 'react-router-dom'
 import './component.css'
 
@@ -14,13 +14,14 @@ export const ActividadForanea = () => {
 
     const [state, setState] = useState({});
     const [interest, setInterest] = useState([]);
-    const {activityid} = useParams();
+    const [collaborators, setCollaborators] = useState([]);
+    const { activityid } = useParams();
 
     const obtenerActividad = async () => {
         const url = `${BACKEND_URL}/activity/${activityid}`;
         const options = {
             method: 'POST',
-            headers: { 'Content-type': 'application/json', 'Accept': 'application/json' },  
+            headers: { 'Content-type': 'application/json', 'Accept': 'application/json' },
         }
         const response = await fetch(url, options);
         const data = await response.json();
@@ -32,7 +33,20 @@ export const ActividadForanea = () => {
         const url = `${BACKEND_URL}/interests`;
         const options = {
             method: 'GET',
-            headers: { 'Content-type': 'application/json', 'Accept': 'application/json' },  
+            headers: { 'Content-type': 'application/json', 'Accept': 'application/json' },
+        }
+
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        return data;
+    }
+
+    const obtenerColaboradores = async () => {
+        const url = `${BACKEND_URL}/collabs?id=${activityid}`;
+        const options = {
+            method: 'GET',
+            headers: { 'Content-type': 'application/json', 'Accept': 'application/json' },
         }
 
         const response = await fetch(url, options);
@@ -42,23 +56,31 @@ export const ActividadForanea = () => {
     }
 
     useEffect(
-       async () => {
-            const data = await obtenerActividad();
-            setState(data);
+        () => {
+            obtenerActividad()
+            .then(data => {
+                setState(data)
+                obtenerIntereses()
+                .then( interests => {
+                    setInterest(interests.filter((i) => data['temas relacionados'].includes(i.id)));
+                })
+                obtenerColaboradores()
+                .then( colaboradores => {
+                    setCollaborators(colaboradores);
+                })
+            })
             
-            const datos = await obtenerIntereses();
-            setInterest(datos.filter((i) => data['temas relacionados'].includes(i.id)));
         }, []);
 
     return (
 
 
-        <Grid container style={{'margin-top': '30px','margin-left': '420px' ,}} className="componente" >
+        <Grid container >
 
-            <Grid container spacing={4}>
+            <Grid container spacing={2}>
 
                 <Grid item xs={12} >
-                    <Typography variant="h2">{state['título']}</Typography>
+                    <h1>{`Detalles de "${state['título']}"`}</h1>
                 </Grid>
 
                 <Grid item xs={12} sm={4} style={{ 'margin-bottom': '25px' }}>
@@ -73,21 +95,21 @@ export const ActividadForanea = () => {
             </Grid>
 
             <Grid container className="separar-arriba">
-                <InformacionActividad 
-                categoria={state['categoria']}
-                tipoActividad={state['tipo']}
-                periodoAcademicoInicio={state['periodo de inicio']}
-                periodoAcademicoFin={state['periodo de finalizacion']}
-                codigo={state['codigoCreador']}
+                <InformacionActividad
+                    categoria={state['categoria']}
+                    tipoActividad={state['tipo']}
+                    periodoAcademicoInicio={state['periodo de inicio']}
+                    periodoAcademicoFin={state['periodo de finalizacion']}
+                    codigo={state['codigoCreador']}
                 />
             </Grid>
 
             <Grid className="separar-arriba">
-                <TemasRelacionados labels= {interest.map(i => i.nombre)} />
+                <TemasRelacionados labels={interest.map(i => i.nombre)} />
             </Grid>
 
             <Grid container className="separar-arriba">
-                <Colaboradores />
+                <Colaboradores collaborators={collaborators.map(c => c.fullName)}/>
             </Grid>
 
             <Grid container className="separar-arriba">
